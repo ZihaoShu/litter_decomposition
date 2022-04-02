75b42dae6e812f8b456bff85f31cd0461a9c590a
shuzihao123 <17320568176@163.com>
2022-04-02 12:35:58 +0800

    哇å完成凋落物附着物呼吸速率曲线拟合

--- src/scripts/litter_respiration.R
+++ src/scripts/litter_respiration.R
@@ -6,43 +6,34 @@
 
 library(tidyverse)
 
-litter_respiration_situ_first <- readxl::read_xlsx("data/raw/litter_respiration_situ_first.xlsx")
-View(litter_respiration_situ_first)
-first_data <- litter_respiration_situ_first[,-c(4,5)] %>% 
-  filter(CO2 != "NA")
-first_data$CO2 <- as.numeric(first_data$CO2)
-first_data$N2O <- as.numeric(first_data$N2O)
-first_data$CH4 <- as.numeric(first_data$CH4)
-View(first_data)
-
-plot_use_model_func <- function(data,x,y,varname,groupnames){
-  data_summary <- function(data,varname,groupnames){
+# 回归计算函数
+plot_use_model_func <- function(data,groupname,typename){
+  data_summary <- function(data){
     require(plyr)
     summary_func <- function(x,col){
       c(mean = mean(x[[col]], na.rm = TRUE),
         sd = sd(x[[col]], na.rm = TRUE))
     }
-    data_sum <- ddply(data, groupnames, .fun = summary_func,
-                      varname)
-    data_sum <- rename(data_sum, c("mean" = varname))
-    return(data_sum)
+    data_sum <- ddply(data, c("Group","Rank"), .fun = summary_func, # 可修改参数，将c("Group","Rank")改为groupnames
+                      "CO2")                                        # 将"CO2"改为varname，在参数中添加这两项
+    data_sum <- rename(data_sum, c("mean" = "CO2"))
   }
-  df <- filter(data,Group == x, Type == y)
-  model_lm <- lm(df,formula = CO2 ~ Rank)
+  model_lm <- filter(data,Group == groupname, Type == typename) %>% 
+    lm(formula = CO2 ~ Rank)
   l <- list(a = as.numeric(format(coef(model_lm)[1], digits = 4)),
             b = as.numeric(format(coef(model_lm)[2], digits = 4)),
             r2 = format(summary(model_lm)$r.squared, digits = 4),
             p = format(summary(model_lm)$coefficients[2,4], digits = 4))
   eq <- substitute(italic(y) == a %.% italic(x) + b~","~italic(R)^2~"="~r2~","~italic(P)~"="~p, l)
-  plot_data1 <- filter(data,Type == y)
-  plot_data2 <- data_summary(plot_data1,varname,groupnames) 
-  plot_data3 <- filter(plot_data2,Group == x)
-  p <- ggplot(plot_data3,aes(x = Rank,y = CO2))+
+  p <- filter(data,Type == typename) %>% 
+    data_summary() %>% 
+    filter(Group == groupname) %>% 
+    ggplot(aes(x = Rank,y = CO2))+
     geom_point(position = position_dodge(0.1),size = 3)+
     geom_errorbar(aes(ymin = CO2 - sd, ymax = CO2 + sd),
                   width = 0.6, position = position_dodge(0.1),size = 0.6)+
     geom_smooth(method = "lm",formula = y~x)+
-    geom_text(aes(x = 15, y = 2500, label = as.character(as.expression(eq))), parse = TRUE,size = 6)+
+    geom_text(aes(x = 50, y = 500, label = as.character(as.expression(eq))), parse = TRUE,size = 6)+
     theme_classic()+
     theme(
       legend.position = c(0.63,0.25),
@@ -52,14 +43,62 @@ plot_use_model_func <- function(data,x,y,varname,groupnames){
     )+
     labs(x = "Time(min)",
          y = "CO2(ppm)",
-         title = x)
-  p
-}
-CK_stem <- plot_use_model_func(data = first_data,x = "P1",y = "stem",
-                               varname = "CO2",groupnames = c("Group","Rank"))
-CK_stem
+         title = groupname)
+  return(p)
+} 
 
+# 2022/3/22第一次取样数据拟合
+litter_respiration_situ_first <- readxl::read_xlsx("data/raw/litter_respiration_situ_first.xlsx")
+first_data <- litter_respiration_situ_first[-c(16,17,20,31,32,40,116,208,216),] %>% 
+  filter(CO2 != "NA")
+first_data$CO2 <- as.numeric(first_data$CO2)
+first_data$N2O <- as.numeric(first_data$N2O)
+first_data$CH4 <- as.numeric(first_data$CH4)
+## 茎凋落物附着呼吸拟合
+ggsave("output/plots/litter_respiration/first_CK_stem.png",
+       plot_use_model_func(data = first_data,groupname = "CK",typename = "stem"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_N1_stem.png",
+       plot_use_model_func(data = first_data,groupname = "N1",typename = "stem"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_N2_stem.png",
+       plot_use_model_func(data = first_data,groupname = "N2",typename = "stem"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_P1_stem.png",
+       plot_use_model_func(data = first_data,groupname = "P1",typename = "stem"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_P2_stem.png",
+       plot_use_model_func(data = first_data,groupname = "P2",typename = "stem"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_NP1_stem.png",
+       plot_use_model_func(data = first_data,groupname = "NP1",typename = "stem"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_NP2_stem.png",
+       plot_use_model_func(data = first_data,groupname = "NP2",typename = "stem"),
+       width = 15,height = 12,dpi = 300)
 
+## 叶凋落物附着物呼吸拟合
+ggsave("output/plots/litter_respiration/first_CK_leaf.png",
+       plot_use_model_func(data = first_data,groupname = "CK",typename = "leaf"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_N1_leaf.png",
+       plot_use_model_func(data = first_data,groupname = "N1",typename = "leaf"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_N2_leaf.png",
+       plot_use_model_func(data = first_data,groupname = "N2",typename = "leaf"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_P1_leaf.png",
+       plot_use_model_func(data = first_data,groupname = "P1",typename = "leaf"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_P2_leaf.png",
+       plot_use_model_func(data = first_data,groupname = "P2",typename = "leaf"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_NP1_leaf.png",
+       plot_use_model_func(data = first_data,groupname = "NP1",typename = "leaf"),
+       width = 15,height = 12,dpi = 300)
+ggsave("output/plots/litter_respiration/first_NP2_leaf.png",
+       plot_use_model_func(data = first_data,groupname = "NP2",typename = "leaf"),
+       width = 15,height = 12,dpi = 300)
 
-
+## 呼吸速率计算及差异性检验
 
